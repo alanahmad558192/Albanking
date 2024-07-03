5209a9fc6c1862496d549a53876f8f1b9b9f88d3
alanahmad558192 <alanahmad81@yahoo.co.uk>
2024-07-03 19:27:30 +0800

    implement initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const SignIn = () => {
+  return <div>SignIn</div>;
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUp;
