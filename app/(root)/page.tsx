cb3e55f7c70612caa41ecb67fe7d6287f8d992a8
alanahmad558192 <alanahmad81@yahoo.co.uk>
2024-07-13 16:52:35 +0800

    Implement the UI (Sidebar, RightSideBar, MobileNav, BankCard)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 /** @format */
 
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Alan" };
+  const loggedIn = { firstName: "Alan", lastName: 'Ahmad', email: 'alanahmad@example.com' };
   return (
     <section className="home">
       <div className="home-content">
@@ -18,12 +19,15 @@ const Home = () => {
           />
 
           <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 123.50},{ currentBalance: 200.00}]} />
     </section>
   );
 };
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -16,7 +17,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 const MobileNav = ({ user }: MobileNavProps) => {
-    const pathname = usePathname(); 
+  const pathname = usePathname();
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
@@ -39,32 +40,51 @@ const MobileNav = ({ user }: MobileNavProps) => {
               height={34}
               alt="Horizon logo"
             />
-            <h1 className="text-26 font-bold font-ibm-plex-serif text-black-1">Horizon</h1>
+            <h1 className="text-26 font-bold font-ibm-plex-serif text-black-1">
+              Horizon
+            </h1>
           </Link>
-          {sidebarLinks.map((item) => {
-            const isActive =
-              pathname === item.route || pathname.startsWith(`${item.route}/`);
-            return (
-              <Link
-                href={item.route}
-                key={item.label}
-                className={cn("sidebar-link", {
-                  "bg-bank-gradient": isActive,
-                })}>
-                <div className="relative size-6">
-                  <Image
-                    src={item.imgURL}
-                    alt={item.label}
-                    fill
-                    className={cn({ "brightness-[3] invert-0": isActive })}
-                  />
-                </div>
-                <p className={cn("sidebar-label", { "!text-white": isActive })}>
-                  {item.label}
-                </p>
-              </Link>
-            );
-          })}
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}>
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+                        <p
+                          className={cn("text-11 font-semibold text-black-2", {
+                            "!text-white": isActive,
+                          })}>
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+
+                USER
+              </nav>
+            </SheetClose>
+
+            FOOTER
+          </div>
         </SheetContent>
       </Sheet>
     </section>
